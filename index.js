4172a3a275802bc2b52e94a0733f8a179497e774
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2023-10-31 13:10:36 -0400

    moved modular question folder into /lib

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-const fs = require('fs');
-const path = require('path');
-const inquirer = require('inquirer');
-const questions = require('./Questions/question'); 
-const generateShape = require('./Questions/generateShapeModule');
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+const questions = require("./lib/Questions/question");
+const generateShape = require("./lib/Questions/generateShapeModule");
 
 // write to file functions
 function writeToFile(fileName, data) {
-    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
-  }
-  // run the page on call
-  function runIt() {
-    inquirer.prompt(questions).then((responses) => {
-      console.log(responses);
-      console.log(`It Worked!`);
-      console.log(`SVG LOGO and file Generated!`);
-      writeToFile("./examples/logo.svg", generateShape({ ...responses }));
-    });
-  }
-  // let's go and
-  runIt();
-
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+// run the page on call
+function runIt() {
+  inquirer.prompt(questions).then((responses) => {
+    console.log(responses);
+    console.log(`It Worked!`);
+    console.log(`SVG LOGO and file Generated!`);
+    writeToFile("./examples/logo.svg", generateShape({ ...responses }));
+  });
+}
+// let's go and
+runIt();
